77028004f91bc08f0ec0ef338d90ea0f01c8b846
Jc <coljiangchao@gmail.com>
2018-05-14 23:20:17 +0800

    fininshed prase mail

--- lib/Bbs/Advertising/CLIS/Cmd/Mission.pm
+++ lib/Bbs/Advertising/CLIS/Cmd/Mission.pm
@@ -11,6 +11,9 @@ use Data::Dumper;
 use DateTime;
 use Mail::IMAPClient;
 use Email::MIME;
+use utf8;
+use Encode qw/encode /;
+use IO::All;
 
 
 # VERSION
@@ -113,6 +116,7 @@ sub BUILD {
 
 sub execute {
     my ($self, $args_ref, $chain_ref) = @_;
+    my ( @need_mail, $output );
     $self->options_usage unless (@$args_ref);
     $self->log->info('call CLI : create mission file');
     ##get mail
@@ -134,13 +138,66 @@ sub execute {
     $self->log->info( "Select mail from ", $self->day, ' day ago');
 	$imap->select('INBOX');
 	my @msgs = $imap->since($dt->epoch - 3600 *24 * $self->day);
+    ##Prase mail
     for my $msg ( @msgs ){
 	    my $str     = $imap->message_string($msg);
 	    my $con     = Email::MIME->new($str);
         my %header  = $con->header_str_pairs;
-	    say Dumper \%header;die;
-	}
+        my $m_regex = $self->title;
+        if ( $header{Subject} &&
+             $header{Subject} =~ /$m_regex/i
+           ) {
+            my(%content, @parts, @attachments);
+            $self->log->info( sprintf "Prase Mail : %s from %s",
+            $header{Subject}, $header{From});
+            @parts = $con->parts;
+            my $mail_body = shift @parts;
+            my $sp_str    = encode('utf8', '分类：|主题：|内容：');
 
+            #my @t = split /分类：|主题：|内容：/, $mail_body;
+            my @mail_content = split /$sp_str/, $mail_body->body;
+             for (@mail_content) {
+                s/^\n+$|^\s+|\s+$//mg;
+                say $1, '$1 Match' if $1;
+             };
+             ($content{type}, $content{title}, $content{body}) = @mail_content[1..3];
+             $content{body} = (split /--/, $content{body})[0];
+            #           say Dumper $con->parts;
+            for (my $i=0;$i<@parts; $i++){
+                 if ( $parts[$i]->content_type =~ /jpeg/ ) {
+                    my $id   = $parts[$i]->content_type =~ /name="(.*)"/ ?
+                               $1 :
+                               $_->content_type;
+                    my $name = sprintf "%s_%s", $header{Subject}, $id;
+                    my $path = io->catfile($self->path, $name);
+                    $parts[$i]->body > $path;
+                    $self->log->info("Save attachment file : ", $path);
+                    push @attachments, $path->absolute->pathname;
+                }
+            };
+            $content{attachments} = \@attachments
+            if @attachments;
+            push @need_mail, \%content;
+        }
+	}
+    ##output
+    for ( my$i=0; $i<@need_mail; $i++ ) {
+        my $mail_c = $need_mail[$i];
+        $output .= $i.'#' x 20 ."\n";
+        my @keys = keys %{$mail_c};
+        my @sort = qw/ body type title /;
+        @sort    =  @keys == 4 ? (@sort, 'attachments') :
+                    @sort;
+#        say Dumper $mail_c;die;
+        my @list = map { $_,  ref $mail_c->{$_} ?
+                              join ",", @{$mail_c->{$_}}
+                              :  $mail_c->{$_}
+                       } @sort;
+
+        $output .= sprintf "%s:%s\n" x @sort, @list;
+    }
+    $output > io($self->mission);
+    $self->log->info("Save Prase result to ", $self->mission);
 }
 
 
