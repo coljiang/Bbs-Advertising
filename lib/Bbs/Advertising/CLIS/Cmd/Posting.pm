801622c9e6ef7205760604492888397668b1ca48
Jc <coljiangchao@gmail.com>
2018-05-18 00:06:30 +0800

    tem save

--- lib/Bbs/Advertising/CLIS/Cmd/Posting.pm
+++ lib/Bbs/Advertising/CLIS/Cmd/Posting.pm
@@ -9,6 +9,7 @@ use Log::Log4perl;
 use Bbs::Advertising;
 use Data::Dumper;
 use DateTime;
+use IO::All;
 
 # VERSION
 # ABSTRACT: This is subroutine of CLI apps that posting bulletin
@@ -19,6 +20,7 @@ option 'mission' =>(
     isa   => Str,
     short    => 'm',
     required => 1,
+    format => 's',
     doc   => 'Mission file include parse mail result'
 );
 
@@ -42,9 +44,32 @@ sub BUILD {
 
 sub execute {
     my ($self, $args_ref, $chain_ref) = @_;
+    my ($mission, @list, @need_content );
     $self->options_usage unless (@$args_ref);
     $self->log->info("call CLI : Posting");
-    $self->log->debug( "read mission file" );
+    $self->log->debug( "read mission file", $self->mission );
+    $mission = io($self->mission)->slurp;
+    @list    = split /[\d0]#+/, $mission;
+    #delete space line
+    @list = grep { /body/  } @list;
+    for my $part ( @list ) {
+        my %save;
+        my @info = split /body:|type:|title:|attachments:/, $part;
+        shift @info;
+        ($save{body}, $save{type}, $save{title}, $save{attachments}) =
+        @info;
+        for (keys %save) {
+            chomp($save{$_}) if $save{$_}
+        }
+        if ( $save{type} =~ /Error/ ) {
+            $self->log->error(
+              sprintf "%s -- %s : error", $save{title}, $save{type}
+                             );
+            die sprintf "%s -- %s : error", $save{title}, $save{type};
+        }
+        push @need_content, \%save;
+    }
+
     my $ad = Bbs::Advertising->new( {
             'log_conf'  =>  $self->log_conf,
             'mission'   =>  $self->mission,
